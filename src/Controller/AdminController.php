18161d54e5e1b68a7bab938b090ec5e0dd9ecf70
Dolupranne <ludovicgodier@hotmail.fr>
2020-05-07 19:43:49 +0200

    ajout membre et admin contenue

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditUserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="admin_")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+
+    /**
+     * Liste des utilisateurs du site
+     * 
+     * @Route("/utilisateurs", name="utilisateurs")
+     */
+    public function usersList(UserRepository $users)
+    {
+        return $this->render('admin/user.html.twig', [
+            'users' => $users->findAll(),
+        ]);
+    }
+
+
+    /**
+     * Modifier un utilisateur
+     * 
+     * @Route("/utilisateurs/modifier/{id}", name="modifier_utilisateur")
+     */
+    public function editUser(User $user, Request $request)
+    {
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+    
+            $this->addFlash('message', 'Utilisateur modifié avec succès');
+            return $this->redirectToRoute('admin_utilisateurs');
+        }
+        
+        return $this->render('admin/edituser.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
+    }
+
+
+}
--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Users;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -41,7 +41,7 @@ class EditUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Users::class,
+            'data_class' => User::class,
         ]);
     }
 }
